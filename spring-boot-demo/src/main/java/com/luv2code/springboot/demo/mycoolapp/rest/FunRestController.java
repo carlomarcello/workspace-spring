bf223791210a171a7aa26b6148622361d5dc7693
Carlo Marcello <carlo.marcello@gmail.com>
2019-08-23 23:04:16 -0300

    Alteracoes no application.properties

--- spring-boot-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ spring-boot-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -2,14 +2,26 @@ package com.luv2code.springboot.demo.mycoolapp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${coach.team}")
+	private String teamName;
 
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello World! Time on server is " + LocalDateTime.now();
+	}
+	
+	@GetMapping("/team")
+	public String teamInfo() {
+		return "Coach name: " + coachName + " | Team name: " + teamName;
 	}
 }
